45612d736e4a9263a3b3986d0e12b95d35a5c22c
KJha02 <kunal.a.jha.24@dartmouth.edu>
2022-11-04 02:13:04 -0400

    Fixed broadcaster modified world and made cycle assignment
    Put 4 robots in default quadrant starting positions. implemented algorithm worked on with andrew in ROS, although movement is still a little buggy

--- wolfpack/run6.sh
+++ wolfpack/run6.sh
@@ -5,5 +5,3 @@ ROS_NAMESPACE=robot_0 rosrun wolfpack follower &
 ROS_NAMESPACE=robot_1 rosrun wolfpack follower &
 ROS_NAMESPACE=robot_2 rosrun wolfpack follower &
 ROS_NAMESPACE=robot_3 rosrun wolfpack follower &
-ROS_NAMESPACE=robot_4 rosrun wolfpack follower &
-ROS_NAMESPACE=robot_5 rosrun wolfpack follower &
